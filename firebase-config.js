fad3ba17fefc60afb810d2f924d11f135880e8b6
yogesh390 <yogesh390@gmail.com>
2025-02-23 18:36:27 +0545

    first commit

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+//11.3.1
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDcbsM-EzVP3XGvjhbW1Uq3NrfFDL2dLsc",
+  authDomain: "yogesh-3efb4.firebaseapp.com",
+  projectId: "yogesh-3efb4",
+  storageBucket: "yogesh-3efb4.firebasestorage.app",
+  messagingSenderId: "163481207003",
+  appId: "1:163481207003:web:b4fb6b535ad4e59e1c0cfe",
+  measurementId: "G-N23P011LTK",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
